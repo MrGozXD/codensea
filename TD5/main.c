3e250790bacad7547d0b04247edd96e90fbf32d3
MrGozXD <yannis.peloutier@ensea.fr>
2021-10-29 16:55:18 +0200

    1st commit TD5

---
+++ TD5/main.c
@@ -0,0 +1,8 @@
+#include "structures.h"
+
+int main(int argc, char const *argv[])
+{
+    NINJA nin1={"Sasuke", 1500};
+    NINJA nin2={"RockLee", 500};
+    return 0;
+}
---
+++ TD5/structures.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stlib.h>
+
+#define NBLETTRES 20
+
+typedef struct ninja
+{
+    char nom[NBLETTRES+1];
+    int chakra;
+}NINJA;
+
+/* Equivalent à :
+
+struct ninja
+{
+    char nom[NBLETTRES+1];
+    int chakra;
+};
+
+typedef struct ninja NINJA;
+
+*/
+
