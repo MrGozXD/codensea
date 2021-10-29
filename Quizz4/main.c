d7ebee34f52394cb35b2799b3862a71dbf63375d
MrGozXD <yannis.peloutier@ensea.fr>
2021-10-29 13:05:56 +0200

    Init Quizz4

---
+++ Quizz4/main.c
@@ -0,0 +1,7 @@
+#include "quizz.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ Quizz4/quizz.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NOM_LENGTH 50
+#define MAX_LIST_SIZE 20
+
+typedef struct etudiant
+{
+    int id;
+    char nom[MAX_NOM_LENGTH];
+    int note;
+}StructEtu;
+
+typedef StructEtu ListStructEtu[MAX_LIST_SIZE];
+
+void entrerDonnees(ListStructEtu list, int s);
