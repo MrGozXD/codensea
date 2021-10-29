aae77ad3e16a2f496b8e6fb39f148b497d82760a
MrGozXD <yannis.peloutier@ensea.fr>
2021-10-29 14:19:54 +0200

    main avec tri

--- Quizz4/main.c
+++ Quizz4/main.c
@@ -6,7 +6,10 @@ int main(int argc, char const *argv[])
     int n;
     n=demanderNbEtudiants();
     entrerDonnees(data, n);
-    printf("\nDonnees entrees\n\n");
+    printf("\nDonnees avant le tri\n\n");
+    afficherDonnees(data,n);
+    trierDonnees(data,n);
+    printf("\nDonnees après le tri\n\n");
     afficherDonnees(data,n);
     return 0;
 }
