488a22cae9cf5f210cd689dd297cf354f4af7a98
yannis.peloutier@ensea.fr <yannis.peloutier@ensea.fr>
2023-01-09 16:47:25 +0100

    Q4 : it works

--- EnseaSH/enseaShQ3.c
+++ EnseaSH/enseaShQ3.c
@@ -3,44 +3,49 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 256
+#define MAX_SHELL_MSG_SIZE 128
 
 int main(int argc, char *argv[])
 {
     const char *welcomeMessage = "Welcome to ENSEA Tiny Shell. \n"
-                                     "To quit, type 'exit'. \n";
-    const char *prompt = "enseash % \n";
-    const char *exitMessage="Bye bye! \n";
-    const char 
+                                 "To quit, type 'exit'. \n";
+    const char *prompt = "enseash % ";
+    const char *exitMessage = "Bye bye! \n";
+
+    char *shellmsg = malloc(MAX_SHELL_MSG_SIZE);
+    char *previousCommandExitStatus = malloc(MAX_SHELL_MSG_SIZE / 4);
+    // shellmsg="";
     char command[BUFFER_SIZE];
+    int previousCommandIsExit = 0;
+    int previousCommandIsSignal = 0;
+    char *previousCommandNumberStatus = malloc(10);
+
+    // Welcome message
+    ssize_t bytesWritten = write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
+    if (bytesWritten == -1)
+    {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
+    bytesWritten = write(STDOUT_FILENO, prompt, strlen(prompt));
+    if (bytesWritten == -1)
+    {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
     while (1)
     {
-        
+
         // clear command[] buffer
         memset(command, 0, BUFFER_SIZE);
 
-        // Welcome message
-        ssize_t bytesWritten = write(STDOUT_FILENO, welcomeMessage, strlen(welcomeMessage));
-        if (bytesWritten == -1)
-        {
-            perror("write");
-            exit(EXIT_FAILURE);
-        }
-
-        // Prompt
-        
-        bytesWritten = write(STDOUT_FILENO, prompt, strlen(prompt));
-        if (bytesWritten == -1)
-        {
-            perror("write");
-            exit(EXIT_FAILURE);
-        }
-
-        
-
         // Read user input
-        
+
         ssize_t bytesRead = read(STDIN_FILENO, command, BUFFER_SIZE);
         if (bytesRead == -1)
         {
@@ -50,15 +55,15 @@ int main(int argc, char *argv[])
 
         // Remove the newline character at the end of the input
         //  command[bytesRead - 1] = '\0'; !! this is not working, it doesn't cover the case command="\0" !!
-        command[strcspn(command,"\n")]=0;
+        command[strcspn(command, "\n")] = 0;
 
         // Check if the user wants to quit
-        
 
         if (strcmp(command, "exit") == 0 || strcmp(command, "\0") == 0)
         {
-            ssize_t bytesWritten2=write(STDOUT_FILENO, exitMessage, strlen(exitMessage));
-            if (bytesWritten2==-1) {
+            ssize_t bytesWritten2 = write(STDOUT_FILENO, exitMessage, strlen(exitMessage));
+            if (bytesWritten2 == -1)
+            {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
@@ -66,8 +71,12 @@ int main(int argc, char *argv[])
         }
 
         // Execute the command with execlp in a child process
-        pid_t pid = fork();
 
+        int status;
+        struct timespec start, end;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        pid_t pid = fork();
         if (pid == -1)
         {
             perror("fork");
@@ -77,7 +86,8 @@ int main(int argc, char *argv[])
         if (pid == 0)
         {
             // child process
-            int status = execlp(command, command, NULL);
+            status = execlp(command, command, NULL); // execlp écrase le reste du code, plus rien n'est exécuté après
+
             if (status == -1)
             {
                 perror("execlp");
@@ -87,7 +97,45 @@ int main(int argc, char *argv[])
         else
         {
             // parent process
+            wait(&status);
+            char str[100];
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            if (WIFEXITED(status))
+            {
+                sprintf(str, "enseaSH [exit : %d]", status);
+                strcat(str, " % ");
+                write(STDOUT_FILENO, str, strlen(str));
+            }
+            else if (WIFSIGNALED(status))
+            {
+                sprintf(str, "enseaSH [signal : %d]", status);
+                strcat(str, " % ");
+                write(STDOUT_FILENO, str, strlen(str));
+            }
+            else
+            {
+                sprintf(str, "enseaSH [unknown : %d]", status);
+                strcat(str, " % ");
+                write(STDOUT_FILENO, str, strlen(str));
+            }
 
+            /*
+            previousCommandIsExit = WIFEXITED(status);
+            previousCommandIsSignal = WIFSIGNALED(status);
+            if(previousCommandIsExit){
+                previousCommandExitStatus = "exit : ";
+                int a=sprintf(previousCommandNumberStatus, "%d", WEXITSTATUS(status));
+                previousCommandExitStatus = strcat(previousCommandExitStatus, previousCommandNumberStatus);
+            }
+            else if(previousCommandIsSignal){
+                previousCommandExitStatus = "signal: ";
+                int a=sprintf(previousCommandNumberStatus, "%d", WTERMSIG(status));
+                previousCommandExitStatus = strcat(previousCommandExitStatus, previousCommandNumberStatus);
+            }
+            else{
+                previousCommandExitStatus = "unknown ";
+            }
+            */
         }
     }
 }
